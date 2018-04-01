93950439a51bc6ea50b30c65e137c3051482f17b
Pekka Hyvönen <pekka@vaadin.com>
2018-04-01 16:48:43 +0300

    Initial commit after generation

---
+++ README.md
@@ -0,0 +1,14 @@
+# wysiwyg-e
+
+Vaadin 10 Java integration of https://github.com/miztroh/wysiwyg-e
+
+## Development instructions
+
+Starting the test/demo server:
+```
+mvn jetty:run
+```
+
+This deploys demo at http://localhost:8080
+
+
---
+++ src/main/java/org/vaadin/pekka/WysiwygE.java
@@ -0,0 +1,13 @@
+package org.vaadin.pekka;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+
+@Tag("wysiwyg-e")
+@HtmlImport("bower_components/wysiwyg-e/wysiwyg-e.html")
+public class WysiwygE extends Component {
+
+    public WysiwygE() {
+    }
+}
